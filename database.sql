8ad2d0296834f92a01aa93a21571e458e2fe2a60
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-02-23 23:06:21 -0300

    definindo estrutura da tabelas de usuarios e reportes no .sql

---
+++ database.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE kingplay;
+USE kingplay;
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `last name` varchar(70) NOT NULL,
+  `intranet_login` varchar(25) UNIQUE NOT NULL,
+  `department` enum(
+    'n1',
+    'n2',
+    'desenvolvimento',
+    'infraestrutura',
+    'cobrança',
+    'comercial',
+    'financeiro',
+    'marketing',
+    'sucesso do cliente'
+  ) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `report` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `post` varchar(255) NOT NULL,
+  `helpful` boolean,
+  `description` varchar(5555) NOT NULL,
+  `approved` boolean,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES user(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
