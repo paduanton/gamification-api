331a217bd8a8e7cce3a860c940537b1c1f88069c
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-03-09 17:01:18 -0300

    ajustando responses UserController

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -3,24 +3,25 @@ import ReportsModel from '../models/reports'
 const model = new ReportsModel();
 
 export function getReports(request, response) {
-    if (request.body != null) {
-        response.status(400).json({
+
+    if (Object.keys(request.body).length !== 0) {
+        return response.status(400).json({
             error: "GET request must not have a body"
         });
     }
 
     model.find((data) => {
         if (Array.isArray(data) === false) {
-            response.status(400).json({
+            return response.status(400).json({
                 error: "can't reach database"
             });
         }
 
         if (data.length === 0) {
-            response.status(404);
+            return response.status(404).json({});
         }
 
-        response.status(200).json(data);
+        return response.status(200).json(data);
     });
 }
 
@@ -28,76 +29,83 @@ export function getReport(request, response) {
     const id = request.params.id;
 
     if (id != Number(id)) {
-        response.status(400).json({
+        return response.status(400).json({
             error: "id param must be int"
         });
     }
 
-    if (request.body != null) {
-        response.status(400).json({
+    if (Object.keys(request.body).length !== 0) {
+        return response.status(400).json({
             error: "GET request must not have a body"
         });
     }
 
     model.findById(id, (data) => {
-
-        if (data == null) {
-            response.status(404);
+        if (!data) {
+            return response.status(404).json({});
         }
-        response.status(200).json(data);
+        return response.status(200).json(data);
     });
 }
 
 export function postReport(request, response) {
     const requestBody = request.body;
 
-    if (requestBody == null) {
-        response.status(400).json({
+    if (Object.keys(requestBody).length === 0) {
+        return response.status(400).json({
             error: "POST request must have a body"
         });
     }
 
     if (requestBody.constructor.name !== "Object") {
-        response.status(400).json({
+        return response.status(400).json({
             error: "request body is not properly formated"
         });
     }
 
     const newModel = new ReportsModel(requestBody);
     newModel.save((data) => {
-        if (data.code) {
-            response.status(400).json({
+
+        if(!data) {
+            return response.status(500).json({
+                error: "can't post data"
+            });
+        }
+
+        if (data.hasOwnProperty("code")) {
+            return response.status(400).json({
                 code: data.errno,
                 message: data.sqlMessage,
                 serverMessage: data.code
             });
         }
+
         if (data.id) {
-            response.status(201).json(data);
+            return response.status(201).json(data);
         }
 
-        response.status(503).json(data);
+        return response.status(503).json(data);
     });
 }
 
 export function putReport(request, response) {
     const requestBody = request.body;
     const id = request.params.id;
-    
-    if (requestBody == null) {
-        response.status(400).json({
+
+    if (Object.keys(requestBody).length === 0) {
+        return response.status(400).json({
             error: "PUT request must have a body"
         });
     }
 
     if (id != Number(id)) {
-        response.status(400).json({
+        return response.status(400).json({
             error: "id param must be int"
         });
     }
 
     if (requestBody.constructor.name !== "Object") {
-        response.status(400).json({
+        return response.status(400).json({
             error: "request body is not properly formated"
         });
     }
@@ -105,14 +113,14 @@ export function putReport(request, response) {
     const newModel = new ReportsModel(requestBody);
     newModel.findOneAndUpdate(id, (data) => {
         if (data.affectedRows === 1) {
-            response.status(204);        
+            return response.status(204);
         }
 
         if (data.affectedRows === 0) {
-            response.status(404).json();
+            return response.status(404).json();
         }
 
-        response.status(503).json(data);
+        return response.status(503).json(data);
     });
 }
 
@@ -120,26 +128,26 @@ export function deleteReport(request, response) {
     const id = request.params.id;
 
     if (id != Number(id)) {
-        response.status(400).json({
+        return response.status(400).json({
             error: "id param must be int"
         });
     }
 
-    if (request.body != null) {
-        response.status(400).json({
+    if (Object.keys(request.body).length !== 0) {
+        return response.status(400).json({
             error: "DELETE request must not have a body"
         });
     }
 
     model.remove(id, (data) => {
         if (data.affectedRows === 1) {
-            response.status(204);        
+            return response.status(204);
         }
 
         if (data.affectedRows === 0) {
-            response.status(404).json();
+            return response.status(404).json();
         }
 
-        response.status(503).json(data);
+        return response.status(503).json(data);
     });
 }
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,54 +6,56 @@ export function getUser(request, response) {
     const id = request.params.id;
 
     if (id != Number(id)) {
-        response.status(400).json({
+        return response.status(400).json({
             error: "id param must be int"
         });
     }
 
-    if (request.body != null) {
-        response.status(400).json({
+    if (Object.keys(request.body).length !== 0) {
+        return response.status(400).json({
             error: "GET request must not have a body"
         });
     }
 
     model.findById(id, (data) => {
-
-        if (data == null) {
-            response.status(404);
+        if (!data) {
+            return response.status(404).json({});
         }
-        response.status(200).json(data);
+        return response.status(200).json(data);
     });
 }
 
 export function postUser(request, response) {
     const requestBody = request.body;
 
-    if (requestBody == null) {
+    if (Object.keys(request.body).length === 0) {
         return response.status(400).json({
             error: "POST request must have a body"
         });
     }
 
     if (requestBody.constructor.name !== "Object") {
-        response.status(400).json({
+        return response.status(400).json({
             error: "request body is not properly formated"
         });
     }
 
     const newModel = new UsersModel(requestBody);
     newModel.save((data) => {
-        if (data.code) {
-            response.status(400).json({
+        if(!data) {
+            return response.status(500).json({
+                error: "can't post data"
+            });
+        }
+
+        if (data.hasOwnProperty("code")) {
+            return response.status(400).json({
                 code: data.errno,
                 message: data.sqlMessage,
                 serverMessage: data.code
             });
         }
-        if (data.id) {
-            response.status(201).json(data);
-        }
 
-        response.status(503).json(data);
+        return response.status(503).json(data);
     });
 }
