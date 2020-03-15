bf6d6eeabeabecba79f43d340c194a3112ef7381
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-03-15 20:43:23 -0300

    adicionando rota para salvar score de reports

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,22 +1,26 @@
 
 import { getReports, getReport, postReport, putReport, deleteReport } from '../controllers/reports'
 import { getUser, postUser } from '../controllers/users'
+import {postReportScore} from '../controllers/usersReportsScore'
 
 const routes = (app) => {
+    app.route('/users')
+        .post(postUser);
+
+    app.route('/users/:id')
+        .get(getUser);
+
     app.route('/reports')
         .get(getReports)
-        .post(postReport)
+        .post(postReport);
 
     app.route('/reports/:id')
         .get(getReport)
         .put(putReport)
-        .delete(deleteReport)
+        .delete(deleteReport);
 
-    app.route('/users')
-        .post(postUser)
-
-    app.route('/users/:id')
-        .get(getUser)
+    app.route('/users/:userId/reports/:reportsId/score')
+        .post(postReportScore);
 }
 
 export default routes
