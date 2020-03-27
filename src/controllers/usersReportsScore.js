1c0678629222174c4deaea6963fdf6580707e954
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-03-27 13:02:49 -0300

    tornando updateUsersLeaderboards() assincrono de definindo retornos

--- src/controllers/usersReportsScore.js
+++ src/controllers/usersReportsScore.js
@@ -5,76 +5,84 @@ import ReportsModel from '../models/reports'
 import e from 'express';
 
 export function postReportScore(request, response) {
-    let UsersReportsScore = {
-        users_id: request.params.usersId,
-        reports_id: request.params.reportsId
-    };
-    const requestBody = request.body;
-
-    if (Object.keys(request.body).length === 0) {
-        return response.status(400).json({
-            error: "POST request must have a body"
-        });
-    }
-
-    if (requestBody.constructor.name !== "Object") {
-        return response.status(400).json({
-            error: "request body is not properly formated"
-        });
-    }
-
-    if (requestBody.approved === false) {
-        const reportsModel = new ReportsModel({ approved: false });
-        reportsModel.findOneAndUpdate(UsersReportsScore.users_id, (data) => {
-            if (data.affectedRows !== 1) {
-                throw new ErrorEvent("Could not update reports table");
-            }
-        });
-
-        return response.status(200).json({
-            message: "users report has been update to not approved"
-        });
-    } else {
-        const reportsModel = new ReportsModel({ approved: true });
-        reportsModel.findOneAndUpdate(UsersReportsScore.users_id, (data) => {
-            if (data.affectedRows !== 1) {
-                throw new ErrorEvent("Could not update reports table");
-            }
-        });
-    }
-
-    const reportsRewards = new ReportsRewards(requestBody.provider, requestBody.approved, requestBody.has_solution)
-    const score = reportsRewards.getScore();
-    UsersReportsScore.value = score.toString();
-
-    const newModel = new UsersReportsScoreModel(UsersReportsScore);
-    newModel.save((data) => {
-        if (!data) {
-            return response.status(500).json({
-                error: "can't POST data"
-            });
-        }
-
-        if (data.hasOwnProperty("code")) {
+    try {
+        let UsersReportsScore = {
+            users_id: request.params.usersId,
+            reports_id: request.params.reportsId
+        };
+        const requestBody = request.body;
+    
+        if (Object.keys(request.body).length === 0) {
             return response.status(400).json({
-                code: data.errno,
-                message: data.sqlMessage,
-                serverMessage: data.code
+                error: "POST request must have a body"
             });
         }
-
-        if (data.id) {
-            data.value = Number(data.value);
-            
-            updateUsersLeaderboards(UsersReportsScore.users_id, data.value, (data)=> {
-                console.log('aqio')
-                console.log(data);
+    
+        if (requestBody.constructor.name !== "Object") {
+            return response.status(400).json({
+                error: "request body is not properly formated"
+            });
+        }
+    
+        if (requestBody.approved === false) {
+            const reportsModel = new ReportsModel({ approved: false });
+            reportsModel.findOneAndUpdate(UsersReportsScore.users_id, (data) => {
+                if (data.affectedRows !== 1) {
+                    throw new ErrorEvent("Could not update reports table");
+                }
+            });
+    
+            return response.status(200).json({
+                message: "users report has been update to not approved"
+            });
+        } else {
+            const reportsModel = new ReportsModel({ approved: true });
+            reportsModel.findOneAndUpdate(UsersReportsScore.users_id, (data) => {
+                if (data.affectedRows !== 1) {
+                    throw new ErrorEvent("Could not update reports table");
+                }
+            });
+        }
+    
+        const reportsRewards = new ReportsRewards(requestBody.provider, requestBody.approved, requestBody.has_solution)
+        const score = reportsRewards.getScore();
+        UsersReportsScore.value = score.toString();
+    
+        const newModel = new UsersReportsScoreModel(UsersReportsScore);
+        newModel.save(async (datas) => {
+            const data = await datas;
+            // if (!data) {
+            //     return response.status(500).json({
+            //         error: "can't POST data"
+            //     });
+            // }
+    
+            if (data.hasOwnProperty("code")) {
+                return response.status(400).json({
+                    code: data.errno,
+                    message: data.sqlMessage,
+                    serverMessage: data.code
+                });
+            }
+    
+            if (data.id) {
+                data.value = Number(data.value);
+    
+                // ---------
+                const teste = await updateUsersLeaderboards(UsersReportsScore.users_id, data.value);
+                console.log(teste)
+                Promise.all(teste).then((result) => {
+                    console.log(result)
+                });
+    
+                // ---------
+    
                 return response.status(201).json(data);
-            });
-
-            return response.status(201).json(data);
-        }
-
-        return response.status(503).json(data);
-    });
+            }
+    
+            return response.status(503).json(data);
+        });
+    } catch (error) {
+        pass
+    }
 }
